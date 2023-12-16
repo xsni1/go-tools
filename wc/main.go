2700fe10b294fd9f62c3230bec771c04b2a880af
xsni1 <thesniboy@gmail.com>
2023-12-16 16:07:00 +0100

    wc tool working

--- wc/main.go
+++ wc/main.go
@@ -1,23 +1,78 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
+	"unicode/utf8"
 )
 
 func main() {
-    c := flag.Bool("c", false, "count bytes")
-    flag.Parse()
-    fileName := flag.Arg(0)
+	c := flag.Bool("c", false, "count bytes")
+	l := flag.Bool("l", false, "count lines")
+	w := flag.Bool("w", false, "count words")
+	m := flag.Bool("m", false, "count chars")
 
-    file, err := os.ReadFile(fileName)
-    if err != nil {
-        fmt.Printf("err reading file: %s\n", err)
-        os.Exit(1)
-    }
+	flag.Parse()
+	fileName := flag.Arg(0)
+	var file *os.File
 
-    if *c {
-        fmt.Printf("%d %s\n", len(file), fileName)
-    }
+	if fileName != "" {
+		f, err := os.Open(fileName)
+
+		if err != nil {
+			fmt.Printf("err reading file: %s\n", err)
+			os.Exit(1)
+		}
+
+		file = f
+	} else {
+		file = os.Stdin
+	}
+
+	lines := 0
+	bytes := 0
+	words := 0
+	chars := 0
+
+	f := bufio.NewReader(file)
+
+	for {
+		line, err := f.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		chars += utf8.RuneCountInString(line)
+		bytes += len(line)
+		lines++
+		w := strings.Fields(line)
+		words += len(w)
+	}
+
+	if !*c && !*l && !*w && !*m {
+		fmt.Printf("%d %d %d %s\n", lines, words, bytes, fileName)
+	}
+
+	output := ""
+
+	if *c {
+		output += fmt.Sprint(bytes)
+	}
+
+	if *l {
+		output += fmt.Sprintf(" %d", lines)
+	}
+
+	if *w {
+		output += fmt.Sprintf(" %d", words)
+	}
+
+	if *m {
+		output += fmt.Sprintf(" %d", chars)
+	}
+
+	fmt.Printf("%s %s\n", output, fileName)
 }
